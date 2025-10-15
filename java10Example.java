47298241b22ed1979c0b14be88423f24eb72d56b
nandhinik17 <118190100+nandhinik17@users.noreply.github.com>
2025-10-15 09:22:03 +0530

    Create java10Example.java

---
+++ java10Example.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class java10Example   {
+
+    public static void main(String[] args) {
+     
+     var name  = "nandhini";
+     System.out.println(name);
+ 
+     
+      
+  }
+}
+
+
+
+
